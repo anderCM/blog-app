3a4f48f47e38e24de1b07becffb3b06e6cc9772b
anderCM <andercm15@gmail.com>
2023-05-10 17:22:17 -0500

    Tests for Like model and methods

---
+++ spec/like_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+describe Like, type: :model do
+  before do
+    @user = User.new(name: 'Anderson', post_counter: 0)
+    @user.save
+    @post = Post.new(author: @user, title: 'How to become a software developer', text: 'The first step is...',
+                     comment_counter: 0, like_counter: 0)
+    @post.save
+    @like = Like.new(user: @user, post: @post)
+    @save_like = @like.save
+  end
+
+  it 'Should save a like' do
+    expect(@save_like).to eq(true)
+  end
+
+  it 'Should increase post counter' do
+    @like.update_counter
+    expect(@post.like_counter).to eq(1)
+  end
+end
