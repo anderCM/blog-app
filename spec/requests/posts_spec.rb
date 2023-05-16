45f25cc60554ca3063aa78634f2ebbefdaf9da10
anderCM <andercm15@gmail.com>
2023-05-16 00:46:44 -0500

    Users specs request added

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-
   describe 'GET #index' do
     let(:user) { FactoryBot.create(:user) }
 
     before do
-      get user_posts_path(user_id: user.id)
+      get user_posts_path(user)
     end
 
     it 'Should return successful response' do
       expect(response).to have_http_status(:success)
-    end 
+    end
 
     it 'Should render the index template' do
       expect(response).to render_template(:index)
