f407ce092c17b486c5faa3babffa447452d9854a
anderCM <andercm15@gmail.com>
2023-05-19 17:22:51 -0500

    Like controller and method to create added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,15 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = current_user.posts.build(post_params)
+    @post.save ? (redirect_to @post) : (render :new)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :author)
+  end
 end
