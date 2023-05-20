c944d43119510af4a8295e1ac1f561f7e2ba20a2
anderCM <andercm15@gmail.com>
2023-05-20 11:15:13 -0500

    Create method in Comment controlled added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :likes, only: [:create]
+      resources :comments, only: [:show, :new, :create]
     end
   end
 end
