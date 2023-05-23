f24457bcb3806a05ae83fd98918a0a921a7ac660
anderCM <andercm15@gmail.com>
2023-05-23 15:28:54 -0500

    Erros un tests fixed

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -14,27 +14,28 @@ RSpec.describe 'Users' do
 
     it 'Should be able to view profile pictures' do
       @users = User.all
-      @users.each  do |user|
+      @users.each do |user|
         expect(page).to have_css("img[src*='#{user.photo}']")
       end
     end
 
     it 'Should be able to see the number of posts each user has written' do
       @users = User.all
-      @users.each  do |user|
+      @users.each do |user|
         expect(page).to have_content("Number of posts: #{user.post_counter}")
       end
     end
 
     it 'Should redirect to the user profile page when clicking on the user name' do
-      click_link('Anderson')
-      expect(page).to have_current_path(user_path(User.first))
+      name_link = find(:css, "a:contains('#{User.last.name}')")
+      name_link.click
+      expect(page).to have_current_path(user_path(User.last))
     end
   end
 
   describe 'User #show' do
     before do
-      visit users_path(User.first.id)
+      visit user_path(User.first.id)
     end
 
     it 'Should be able to view profile picture' do
