daa460ce7e21a6c41daf5c2af55ef956df8d79e0
anderCM <andercm15@gmail.com>
2023-05-23 15:05:34 -0500

    Specs for user index added

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -8,14 +8,4 @@ FactoryBot.define do
     end
     post_counter { 0 }
   end
-
-  factory :user2 do
-    name { 'Sophie' }
-    photo { 'https://media.licdn.com/dms/image/D4E03AQHUp8AJl-0q3g/profile-displayphoto-shrink_400_400/0/1677634492816?e=1689811200&v=beta&t=brB8LkXqB3EGmGLhFiwdJif_3NRMqJj6erfyRYrrUO0' }
-    bio do
-      'Self-taught web developer with over 2 years of experience working on complex software development.
-      I am eager to work on new projects in the near future.'
-    end
-    post_counter { 0 }
-  end
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,12 +1,28 @@
 require 'rails_helper'
 RSpec.describe 'Users' do
   describe 'User #index' do
-    it 'Should render Anderson and Sophie in the index page' do
+    before do
       visit users_path
+    end
+    it 'Should render Anderson and Sophie in the index page' do
       expect(page).to have_content('Anderson')
       expect(page).to have_content('Anderson 2')
       expect(page).to have_content('Anderson 3')
       expect(page).to have_content('Anderson 4')
+    end
+
+    it 'Should be able to view profile pictures' do
+      @users = User.all
+      @users.each  do |user|
+        expect(page).to have_css("img[src*='#{user.photo}']")
+      end
+    end
+
+    it 'Should be able to see the number of posts each user has written' do
+      @users = User.all
+      @users.each  do |user|
+        expect(page).to have_content("Number of posts: #{user.post_counter}")
+      end
     end
   end
 end
