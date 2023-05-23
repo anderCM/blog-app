154fdc0946237bdb39e3abd10766f171417569cd
lucascomamala <lucascomamala@gmail.com>
2023-05-23 14:15:45 -0600

    Specs for Users show page

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Users' do
     before do
       visit users_path
     end
+
     it 'Should render Anderson and Sophie in the index page' do
       expect(page).to have_content('Anderson')
       expect(page).to have_content('Anderson 2')
@@ -24,5 +25,31 @@ RSpec.describe 'Users' do
         expect(page).to have_content("Number of posts: #{user.post_counter}")
       end
     end
+
+    it 'Should redirect to the user profile page when clicking on the user name' do
+      click_link('Anderson')
+      expect(page).to have_current_path(user_path(User.first))
+    end
   end
+
+  describe 'User #show' do
+    before do
+      visit users_path(User.first.id)
+    end
+
+    it 'Should be able to view profile picture' do
+      expect(page).to have_css("img[src*='#{User.first.photo}']")
+    end
+
+    it 'Should be able to view the user\'s name' do
+      expect(page).to have_content(User.first.name)
+    end
+
+    it 'Should be able to see the num of posst the user has written' do
+      expect(page).to have_content("Number of posts: #{User.first.post_counter}")
+    end
+
+    it 'Should be able to see the user\'s bio' do
+      expect(page).to have_content(User.first.bio)
+    end
 end
