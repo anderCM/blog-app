e8129bce486638f680f452266864a50e1c9fb541
Solomon <solobarine@gmail.com>
2023-05-24 16:27:24 +0100

    Add Seeds data for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+6.times { user = User.create!(name: Faker::Name.first_name, photo: Faker::Avatar.image, bio: Faker::Job.title, post_counter: 10) }
+
+User.all.each do |user|
+  10.times do
+    post = Post.create!(
+      title: Faker::Quotes::Shakespeare.hamlet_quote, 
+      text: Faker::Quote.matz,
+      author: user,
+      comment_counter: 0,
+      like_counter: 0
+    )
+
+    10.times do 
+      user = User.order("RANDOM()").first
+      Comment.create!(text: Faker::Quotes::Shakespeare.as_you_like_it_quote, user:, post:)
+    end
+
+    10.times do 
+      user = User.order("RANDOM()").first
+      Like.create!(user:, post:)
+    end
+  end
+end
