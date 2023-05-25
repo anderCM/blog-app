1e0ffb57d5e415741be7ce95890bb6fb321a13d7
lucascomamala <lucascomamala@gmail.com>
2023-05-24 19:01:30 -0600

    Delete post functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root :to => redirect('users')
   devise_for :users
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :likes, only: [:create]
       resources :comments, only: [:show, :new, :create]
     end
