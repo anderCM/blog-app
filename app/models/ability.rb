7e1b72d1317c4a60f7616cb96d8635906521ab77
anderCM <andercm15@gmail.com>
2023-05-24 21:33:14 -0500

    Abilities for users and role added

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,18 +1,18 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     can :read, :all
     can :create, User
-    
+
     return unless user.present?
+
     can :manage, Post, author: user
     can :manage, Comment, user_id: user.id
     can :manage, Like, user_id: user.id
 
     return unless user.is?('admin')
+
     can :manage, :all
   end
 end
