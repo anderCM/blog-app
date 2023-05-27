d4d12bde05188f01af5e6f3e811b46f99db4893a
lucascomamala <lucascomamala@gmail.com>
2023-05-26 18:01:36 -0600

    Like counter

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,6 @@ class LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     @like = Like.new(user: current_user, post: @post)
     if @like.save!
-      @like.update_counter
       redirect_to user_post_path(params[:user_id], params[:post_id]), notice: 'You Liked this post!'
     else
       render :new, status: :unprocessable_entity
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,14 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  def update_counter
+  after_create :increment_like_counter
+  after_destroy :decrement_like_counter
+
+  def increment_like_counter
     post.increment!(:like_counter)
+  end
+
+  def decrement_like_counter
+    post.decrement!(:like_counter)
   end
 end
